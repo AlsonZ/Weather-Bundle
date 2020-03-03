66e4746f015be4a06af7a051e76018288d0f2d38
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-03-03 19:49:30 +1100

    fix scrape_accu bug

--- scraping/scrape_accu.js
+++ scraping/scrape_accu.js
@@ -1,5 +1,5 @@
 exports.getData = async function(page, url) {
-  // page.setUserAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36");
+  page.setUserAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36");
   const daysData = [];
   const days = 7;
   let errorHappened = false;
